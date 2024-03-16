9cbcbce80513cdfa4b68130f4477b76d7c58ca23
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-16 15:42:55 +0400

    Update v2.js

--- v2.js
+++ v2.js
@@ -1,9 +1,6 @@
 setInterval(function() {
   var elements = document.getElementsByTagName('button');
   for (var i = 0; i < elements.length; i++) {
-    if (elements[i].innerText == 'Закрасить') {
-      elements[i].click();
-    }
     if (elements[i].innerText == 'Push Pixel') {
       elements[i].click();
     }
