be5e722d6b3f60cd40264979ee493054236f4293
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-17 14:08:56 +0400

    Archived v2.js"
    This script is not supported anymore, check the repository for a new version!

---
+++ archive_v2.js
@@ -0,0 +1,8 @@
+// setInterval(function() {
+//   var elements = document.getElementsByTagName('button');
+//   for (var i = 0; i < elements.length; i++) {
+//     if (elements[i].innerText == 'Push Pixel') {
+//       elements[i].click();
+//     }
+//   }
+// }, 500);
--- v2.js
+++
@@ -1,8 +0,0 @@
-setInterval(function() {
-  var elements = document.getElementsByTagName('button');
-  for (var i = 0; i < elements.length; i++) {
-    if (elements[i].innerText == 'Push Pixel') {
-      elements[i].click();
-    }
-  }
-}, 500);
