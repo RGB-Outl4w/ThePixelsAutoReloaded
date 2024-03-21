19a9a09f8dc1685e08eeaa59895dace6b04d7709
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-21 19:53:52 +0400

    Updated and renamed TPA_main.js to TPAR_main.js

--- TPAR_main.js
+++ TPAR_main.js
@@ -1,2 +1,2 @@
 // This is the main script you should execute, the link below will be updated when a new version of the script is released so you don't have to change it manually.
-fetch('https://raw.githubusercontent.com/RGB-Outl4w/ThePixelsAuto/main/TPA_v3.x.js').then(response => response.text()).then(script => eval(script));
+fetch('https://raw.githubusercontent.com/RGB-Outl4w/ThePixelsAuto/main/TPAR_v4.x.js').then(response => response.text()).then(script => eval(script));
