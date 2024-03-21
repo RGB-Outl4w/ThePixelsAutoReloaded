973bd0f5e36610cf34811e1846d33f9ed1ef30ef
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-21 19:23:09 +0400

    Create beta_TPAR_v3.3.js

---
+++ test/beta_TPAR_v3.3.js
@@ -0,0 +1,38 @@
+async function checkAndUpdate() {
+    let element = document.querySelector("[class^=\'BlackButtonStyled-sc\']");
+    let count = 0;
+  
+    while (true) {
+      if (count > 10) {
+        break;
+      }
+  
+      if (element) {
+        element.click();
+        count = 0;
+        await wait(2000);
+  
+        let searchText = "magasan";
+        let textOnPage = document.body.textContent;
+  
+        if (!textOnPage.includes(searchText)) {
+          await wait(5000);
+        }
+      } else {
+        await wait(1000);
+      }
+    }
+  }
+  
+  function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  window.alert = function() {}, window.confirm.toString = function() {};
+  
+  const metax = 1077,
+  
+  elements = document.querySelectorAll("[class^=\'PieChartStyled-sc\']");
+  elements = elements.textContent.replace(/(\d{1,2},\d{2}|\d{3})% >/, '');
+  
+  checkAndUpdate();
