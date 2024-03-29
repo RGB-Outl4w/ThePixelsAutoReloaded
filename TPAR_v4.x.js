fad3adbe4a0dc61713617e05a36a2f2cea2a399b
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-29 14:42:38 +0400

    Update TPAR_v4.x.js

--- TPAR_v4.x.js
+++ TPAR_v4.x.js
@@ -4,24 +4,23 @@
  Telegram channels: t.me/mdsays | t.me/rgbtrap
 */
 
-var h = {
-    drawMode: 6
-  };
+var at = {};
 
-window.monkeyPatch = h;
+at.drawMode = 6
+window.monkeyPatch = at
 
+unixTime = Date.now();
 
-document.querySelector("script").remove();
-document.getElementById("root").innerHTML = "";
+document.querySelectorAll('script').forEach((bq) => bq.remove());
+document.getElementById('root').innerHTML = ''
 
-var s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "https://pipyoutube.ru/cats/index-GKUHWpC_.js?1234";
-// s.src = "https://cdn.jsdelivr.net/gh/RGB-Outl4w/ThePixelsAutoReloaded/assets/index-GKUHWpC_.js"; 
-s.type = "module";
-  
+var s = document.createElement('script');
+s.type = 'text/javascript'
+s.src = 'https://pipyoutube.ru/cats/index-GKUHWpC_.js?1234'
+s.type = 'module'
+
 document.head.appendChild(s);
 
 setTimeout(function () {
-    fetch("https://raw.githubusercontent.com/RGB-Outl4w/ThePixelsAutoReloaded/main/releases/release_TPAR_v4.1.js").then(response => response.text()).then(script => eval(script));
+    fetch("https://raw.githubusercontent.com/RGB-Outl4w/ThePixelsAutoReloaded/main/releases/release_TPAR_v4.1.js?", unixTime).then(response => response.text()).then(script => eval(script));
 }, 10000);
