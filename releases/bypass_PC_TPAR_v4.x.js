43a37ee420ecc38b91ba4c6a60fa5568a6ab96a9
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-30 00:21:26 +0400

    Update bypass_PC_TPAR_v4.x.js

--- releases/bypass_PC_TPAR_v4.x.js
+++ releases/bypass_PC_TPAR_v4.x.js
@@ -1,3 +1,9 @@
+/*
+
+This is a standalone script that allows you to bypass the PC block in The Pixels game and removes all the ad pop-ups. The following code can be executed without any additional scripts, it only allows access to the main game without providing any additional functionality except for blocking annoying advertisement.
+
+*/
+
 var unixTime = Date.now();
 
 document.querySelectorAll('script').forEach((func) => func.remove())
