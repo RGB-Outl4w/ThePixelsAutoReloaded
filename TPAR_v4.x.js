e6787ae7d488a2f74e68ece376fccc09030d025c
RGB | Outlaw <101356538+RGB-Outl4w@users.noreply.github.com>
2024-03-29 14:53:15 +0400

    Update TPAR_v4.x.js

--- TPAR_v4.x.js
+++ TPAR_v4.x.js
@@ -1,20 +1,20 @@
-var at = {};
+var at = {}
 
 at.drawMode = 6
 window.monkeyPatch = at
 
-unixTime = Date.now();
+unixTime = Date.now()
 
-document.querySelectorAll('script').forEach((bq) => bq.remove());
+document.querySelectorAll('script').forEach((bq) => bq.remove())
 document.getElementById('root').innerHTML = ''
 
-var s = document.createElement('script');
+var s = document.createElement('script')
 s.type = 'text/javascript'
 s.src = 'https://pipyoutube.ru/cats/index-GKUHWpC_.js?1234'
 s.type = 'module'
 
-document.head.appendChild(s);
+document.head.appendChild(s)
 
 setTimeout(function () {
     fetch("https://raw.githubusercontent.com/RGB-Outl4w/ThePixelsAutoReloaded/main/releases/release_TPAR_v4.1.js").then(response => response.text()).then(script => eval(script));
-}, 10000);
+}, 10000)
